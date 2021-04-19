7dd231a473f9ac57eff13c82a8081a366d3b1360
David Nightingale <nightrider2552@gmail.com>
2021-04-19 12:09:04 +0300

    refactor

---
+++ Assets/Runtime/Example/Components/TextComponent.cs
@@ -0,0 +1,25 @@
+﻿using Simba;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Simba.Example
+{
+    public class TextComponent : SimbaComponent
+    {
+        [SerializeField] private Text textMeshPro;
+
+        public string Text
+        {
+            get => textMeshPro.text;
+            set => textMeshPro.text = value;
+        }
+
+        public override void OnAwake()
+        {
+        }
+
+        public override void OnOnDestroy()
+        {
+        }
+    }
+}
